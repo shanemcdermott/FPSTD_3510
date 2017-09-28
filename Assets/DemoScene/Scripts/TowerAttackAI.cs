15b34a74f49b7d2da332b7be203232acea6e2d2d
xpriab <bishopbrogan@gmail.com>
2017-09-28 15:50:19 -0500

    Tower targets enemy closest to the player

--- Assets/DemoScene/Scripts/TowerAttackAI.cs
+++ Assets/DemoScene/Scripts/TowerAttackAI.cs
@@ -1,16 +1,25 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class TowerAttackAI : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
+
+		GameObject[] enemies = GameObject.FindGameObjectsWithTag ("Enemy");
+		GameObject player = GameObject.FindGameObjectWithTag ("Player");
+
+		float closestDist = float.MaxValue;
+
+		for (int i = 0; i < enemies.Length; i++) {
+			float currDist = (player.transform.position - enemies[i].transform.position).sqrMagnitude;
+			if (currDist < closestDist) {
+				closestDist = currDist;
+				this.GetComponent<TowerAim> ().setTarget (enemies [i]);
+			}
+			
+			
+		}
 		
 	}
 }
