8f1972d18e64bd942880aa8062fe8cba5331297d
xpriab <bishopbrogan@gmail.com>
2017-09-28 16:37:09 -0500

    turret changes
    turret shoots
    targets react to shots fired by turret

--- Assets/DemoScene/Scripts/TowerAttackAI.cs
+++ Assets/DemoScene/Scripts/TowerAttackAI.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class TowerAttackAI : MonoBehaviour {
 
+	static public float timeBetweenShots = 0.5f;
+
+	private float timeUntilNextShot = timeBetweenShots;
+
+	void Start()
+	{
+		this.GetComponentInChildren<LineRenderer> ().enabled = false;
+	}
+
+
 	void FixedUpdate () {
 
 		GameObject[] enemies = GameObject.FindGameObjectsWithTag ("Enemy");
@@ -20,6 +30,24 @@ public class TowerAttackAI : MonoBehaviour {
 			
 			
 		}
-		
+
+		timeUntilNextShot -= Time.deltaTime;
+
+		if (timeUntilNextShot < 0) {
+			StartCoroutine (shoot());
+			timeUntilNextShot += timeBetweenShots;
+		}
+
+	}
+
+
+	private IEnumerator shoot()
+	{
+		this.GetComponentInChildren<LineRenderer> ().enabled = true; //this might not be the best way to do this...
+		yield return new WaitForSeconds (0.2f);
+		Component targetComponent =  this.GetComponent<TowerAim> ().getTarget ().GetComponent<ReactiveTarget> ();
+		if (targetComponent != null)
+			this.GetComponent<TowerAim> ().getTarget ().GetComponent<ReactiveTarget> ().reactToHit ();
+		this.GetComponentInChildren<LineRenderer> ().enabled = false;
 	}
 }
