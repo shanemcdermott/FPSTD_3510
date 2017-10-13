c794f8ff867a728dca69dc7c0b1f938251431e47
jastein693 <jastein@unomaha.edu>
2017-10-12 19:45:03 -0500

    Created HUD and HUDManager

---
+++ Assets/Scripts/Managers/HUDManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUDManager : MonoBehaviour {
+    private PlayerHealth playerHealth;
+    public Slider healthSlider;
+    public Text wave;
+    public Text crystals;
+    public Text upgrades;
+    public Text phase;
+
+	// Use this for initialization
+	void Start () {
+        playerHealth = GetComponent<PlayerHealth>();
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        healthSlider.value = playerHealth.currentHealth;
+	}
+
+
+}
