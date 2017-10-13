df69a6bfae7353dbf4639833346fe135f1de2c37
Shane <shanemcdermott@unomaha.edu>
2017-10-13 01:33:58 -0500

    Added GameManager and SoundManager class

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager instance = null;
+
+    
+    private TileMap tileMap;
+    private int level = 1;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else if(instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+        tileMap = GetComponent<TileMap>();
+
+        InitGame();
+    }
+
+    private void InitGame()
+    {
+        //Prepare level etc.
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+		
+	}
+}
