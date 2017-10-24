2ceefda6d21564df104f25d02f60f6d16504b141
Unknown <bishopbrogan@gmail.com>
2017-10-24 17:31:06 -0500

    Pathfinding moved to the inside of tilemap

--- Assets/Scripts/Environment/MapFactory.cs
+++ Assets/Scripts/Environment/MapFactory.cs
@@ -16,20 +16,20 @@ public class MapFactory : MonoBehaviour
     /// <summary>
     /// Number of tiles in X direction
     /// </summary>
-    public int width = 10;
+    public int width;
     /// <summary>
     /// Number of tiles in Z direction
     /// </summary>
-    public int height = 10;
+    public int height;
 
     /// <summary>
     /// Tile size in meters in X direction.
     /// </summary>
-    public int tileWidth = 2;
+    public int tileWidth = 1;
     /// <summary>
     /// Tile height in meters in Z direction.
     /// </summary>
-    public int tileHeight = 2;
+    public int tileHeight = 1;
 
     private Transform board;
     private TileMap map;
@@ -39,39 +39,53 @@ public class MapFactory : MonoBehaviour
     {
         SetupScene();
 	}
-	
+
+	public void SetupScene()
+	{
+		SetupBoard();
+		//this.GetComponentInChildren<PathFinder> ().setMap (this.map);
+	}
+
     /// <summary>
     /// Populate map with random tiles.
     /// </summary>
     void SetupBoard()
     {
-        GameObject boardObject = new GameObject("Board");
+        GameObject boardObject = new GameObject("TileBoard");
         map = boardObject.AddComponent<TileMap>();
         board = boardObject.transform;
 
-        map.setMapSize(width, height);
+        //map.setMapSize(width, height);
+		map.initTileMap(width, height);
 
         for (int x = 0; x < width; x++)
         {
             for (int z = 0; z < height; z++)
             {
-                GameObject toInstantiate = floorTiles[Random.Range(0, floorTiles.Length)];
+				GameObject toInstantiate = floorTiles[Random.Range (0, floorTiles.Length)];
 				Vector3 p = this.transform.position;
 				GameObject instance = Instantiate(toInstantiate, new Vector3(x * tileWidth + p.x, 0f + p.y, z * tileHeight + p.z), Quaternion.identity) as GameObject;
                 instance.transform.SetParent(board);
                 map.setTileAt(x,z,instance);
+
             }
         }
+
+		addSomeWalls (10);
+
+		Debug.Log ("Path found = " + map.isPath ());
+
+
     }
 
-    public void SetupScene()
-    {
-        SetupBoard();
-		//this.GetComponentInChildren<PathFinder> ().setMap (this.map);
-    }
-
-	public TileMap getTileMap()
+	void addSomeWalls(int numWalls)
 	{
-		return map;
+		for (int i = 0; i < numWalls; i++) {
+			int x = Random.Range (0, width);
+			int z = Random.Range (0, height);
+
+			map.PlaceWallHere (x, z);
+		}
 	}
+ 
 }
--- Assets/Scripts/Environment/TileMap.cs
+++ Assets/Scripts/Environment/TileMap.cs
@@ -5,17 +5,69 @@ using UnityEngine;
 public class TileMap : MonoBehaviour
 {
 
+	public bool giz = true;
     protected GameObject[,] tileMap;
-	private int Xsize;
-	private int Zsize;
 
 
-    public void setMapSize(int numX, int numZ)
-    {
-        tileMap = new GameObject[numX, numZ];
-		Xsize = numX;
-		Zsize = numZ;
-    }
+	private int xlen;
+	private int zlen;
+
+
+	//start point and target for pathfinding
+	private int startx;
+	private int startz;
+	private int targetx;
+	private int targetz;
+
+	//used for pathfinding
+	private bool[,] grid = null;
+	private bool[,] visited = null;
+	private bool[,] path = null;
+
+
+//    //do we need this?
+//    public void setMapSize(int numX, int numZ)
+//    {
+//        tileMap = new GameObject[numX, numZ];
+//		xlen = numX;
+//		zlen = numZ;
+//    }
+
+	//default init method
+	public void initTileMap(int xSize, int zSize)
+	{
+		xlen = xSize;
+		zlen = zSize;
+		tileMap = new GameObject[zlen, xlen];
+
+		setStartTile(0, 0);
+		setTargetTile(xlen - 1, zlen - 1);
+
+		grid = new bool[zlen, xlen];
+		visited = new bool[zlen, xlen];
+		path = new bool[zlen, xlen];
+
+		for (int x = 0; x < xlen; x++) {
+			for (int z = 0; z < zlen; z++) {
+				//grid [z, x] = !(getTileAt (x, z).GetComponent<Tile> ().HasWall ()); //false for can't walk through (maybe change this)
+				grid[z, x] = true;
+				visited[z, x] = false;
+				path [z, x] = false;
+			}
+		}
+	}
+
+	public void setStartTile (int x, int z)
+	{
+		startx = x;
+		startz = z;
+	}
+
+	public void setTargetTile (int x, int z)
+	{
+		targetx = x;
+		targetz = z;
+	}
 
     public void setTileAt(int x, int z, GameObject tile)
     {
@@ -29,12 +81,137 @@ public class TileMap : MonoBehaviour
 
 	public int getWidth()
 	{
-		return Xsize;
+		return xlen;
 	}
 
 	public int getLength()
 	{
-		return Zsize;
+		return zlen;
 	}
 
+	public bool HasWallHere(int x, int z)
+	{
+		return tileMap[z, x].GetComponent<Tile>().HasWall();
+	}
+
+	public void PlaceWallHere(int x, int z)
+	{
+		tileMap [z, x].GetComponent<Tile> ().PlaceWall ();
+	}
+
+	public bool CanPlaceHere(int x, int z)
+	{
+		//check here if you can place...
+		return true;
+	}
+
+	public bool isPath ()
+	{
+		
+		return recursiveFindPath ();
+
+	}
+
+	public bool recursiveFindPath()
+	{
+		grid = new bool[zlen, xlen];
+		visited = new bool[zlen, xlen];
+		path = new bool[zlen, xlen];
+
+		for (int x = 0; x < xlen; x++) {
+			for (int z = 0; z < zlen; z++) {
+				grid [z, x] = !(getTileAt (x, z).GetComponent<Tile> ().HasWall ()); //false for can't walk through (maybe change this)
+				visited[z, x] = false;
+				path [z, x] = false;
+			}
+		}
+
+		return recursiveFindPathHelper(startx, startz);
+	}
+
+	public bool recursiveFindPathHelper(int x, int z)
+	{
+		visited [z, x] = true;
+
+		if (x == targetx && z == targetz)
+			return true;
+
+		if (visitable (x, z + 1) && !visited[z + 1, x]) {
+			if (recursiveFindPathHelper (x, z + 1)) {
+				path [z + 1, x] = true;
+				return true;
+			}
+		}
+
+		if (visitable (x + 1, z) && !visited[z, x + 1]) {
+			if (recursiveFindPathHelper (x + 1, z)) {
+				path [z, x + 1] = true;
+				return true;
+			}
+		}
+
+		if (visitable (x - 1, z) && !visited[z, x - 1]) {
+			if (recursiveFindPathHelper (x - 1, z)) {
+				path [z, x -1] = true;
+				return true;
+			}
+		}
+
+		if (visitable (x, z - 1) && !visited[z - 1, x]) {
+			if (recursiveFindPathHelper (x, z - 1)) {
+				path [z - 1, x] = true;
+				return true;
+			}
+		}
+
+
+		return false;
+
+	}
+
+	private bool visitable(int x, int z)
+	{
+		if (!(x < 0 || x >= xlen || z < 0 || z >= zlen))
+		{
+			return grid [z, x];
+		}
+		return false;
+	}
+
+
+
+	public void OnDrawGizmos()
+	{
+		if (giz == true) {
+			float tileWidth = 1f;
+			Vector3 cubeSize = new Vector3 (tileWidth, tileWidth, tileWidth);
+
+
+			for (int x = 0; x < xlen; x++) {
+				for (int z = 0; z < zlen; z++) {
+
+
+					Vector3 center = new Vector3 (tileWidth * x, tileWidth * 0.5f, tileWidth * z);
+					Gizmos.color = new Color (0.7f, 0.7f, 0.7f);
+
+					if (path != null && grid != null) {
+						if (path [z, x] || (z == startz && x == startx) || (z == targetz && x == targetx)) {
+							Gizmos.color = new Color (1f, 0f, 0f);
+							Gizmos.DrawCube (center, cubeSize / 2);
+							Gizmos.color = new Color (0.7f, 0.7f, 0.7f);
+						}
+
+						if (grid [z, x]) {
+							Gizmos.DrawWireCube (center, cubeSize);
+						} else {
+							Gizmos.DrawCube (center, cubeSize);
+						}
+					}
+
+
+				}
+			}
+		}
+	}
+			
 }
